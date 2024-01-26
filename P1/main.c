20a452a46229039f305bc0971caebd05d0f6595f
Claudia Hereter <claudia.hereter01@estudiant.upf.edu>
2024-01-26 13:03:25 +0100

    verify done

--- P1/main.c
+++ P1/main.c
@@ -1,4 +1,5 @@
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,15 +8,14 @@
 #include "crc.c"
 #include "main.h"
 
-//Calculate CRC
-//use unsigned short for 2 bytes instead of 1
- void generate(char *inputFilename){
 
-    //create name of output file
+//Calculate CRC
+void generate(char *inputFilename){
+
+    // Create CRC filename by appending ".crc" to the input filename
     char crcFilename[256];
     strcpy(crcFilename, inputFilename);
     strcat(crcFilename, ".crc");
-
 
     // Open input file for reading
     int inputFile = open(inputFilename, O_RDONLY);
@@ -24,7 +24,6 @@
         exit(5);
     }
 
-
     // Open or create CRC file for writing with the name created before
     int crcFile = open(crcFilename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
     if (crcFile == -1) {
@@ -32,117 +31,130 @@
         exit(5);
     }
 
-    //initialize the CRC to 0
-    crc result=0;
 
+    // Initialize the CRC to 0
+    crc result=0;
+    // Buffer to read data in chunks
     char buff[256];
     int bytesRead = 0;
+
     // Read input file in blocks of 256 bytes and compute CRC
     while ((bytesRead = read(inputFile, buff, sizeof(buff))) > 0) {
         result = crcSlow((unsigned char*)buff, bytesRead);
-
 
         // Write CRC to CRC file
         write(crcFile, &result, sizeof(result));
     }
 
+    // Close files
     close(inputFile);
     close(crcFile);
 
-
-}
-
-void verify( char *inputFile, int maxNumErrors){
-
-
-
-
-
-
 }
 
 
+void verify(char *inputFilename, int maxNumErrors) {
 
-
-
-
-/*
-
-// Function to verify CRC file
-void verifyCRCFile(char *filename, int maxNumErrors) {
+    // create the name for the output file once again
     char crcFilename[256];
-    strcpy(crcFilename, filename);
+    strcpy(crcFilename, inputFilename);
     strcat(crcFilename, ".crc");
 
-    // Check if the CRC file exists
-    int crcFileExists = access(crcFilename, F_OK) != -1;
-
-    if (!crcFileExists) {
-        fprintf(stderr, "Error: CRC file not found.\n");
-        exit(22);
-    }
-
-    int crcFile = open(crcFilename, O_RDONLY);
-    if (crcFile == -1) {
-        perror("Error opening CRC file");
+    // opening the input file passed as argument for reading
+    int inputFile = open(inputFilename, O_RDONLY);
+    if (inputFile == -1) {
+        printf("Couldn't open input file!");
         exit(5);
     }
 
-    // Read the stored CRC from the CRC file
-    unsigned short storedCRC;
-    read(crcFile, &storedCRC, sizeof(storedCRC));
+    // we assume that the crcFile is already created from the generate function
+    int crcFile = open(crcFilename, O_RDONLY);
+    if (crcFile == -1) {
+        printf("Couldn't open CRC file!");
+        close(inputFile);
+        exit(5);
+    }
 
-    // Calculate the CRC of the original file
-    unsigned short calculatedCRC = calculateCRC(filename);
+    // Initialize the CRC to 0
+    crc result = 0;
+    // Buffer to read data in chunks
+    char buff[256];
+    int bytesRead = 0;
+    int numErrors = 0;
 
-    // Compare the stored CRC with the calculated CRC
-    if (storedCRC == calculatedCRC) {
-        printf("File OK\n");
-    } else {
-        printf("File has errors\n");
+    // Read input file in blocks of 256 bytes and compare CRC
+    while ((bytesRead = read(inputFile, buff, sizeof(buff))) > 0) {
+        // Compute CRC for the current block
+        result = crcSlow((unsigned char*)buff, bytesRead);
 
-        // Check if the number of errors is below the threshold
-        if (maxNumErrors > 0 && (calculatedCRC != storedCRC)) {
-            printf("Number of errors below threshold: %d\n", maxNumErrors);
+        // Read CRC from CRC file
+        crc crcFromFile;
+        if (read(crcFile, &crcFromFile, sizeof(crc)) != sizeof(crc)) {
+            printf("Error reading CRC from file!");
+            close(inputFile);
+            close(crcFile);
+            exit(5);
+        }
+
+        // Compare CRC values
+        if (result != crcFromFile) {
+            numErrors++;
+            // Check if there are errors and print the result
+            if (numErrors > maxNumErrors) {
+                printf("file has errors\n");
+                close(inputFile);
+                close(crcFile);
+                exit(0);
+            } else {
+                printf("file OK\n");
+            }
         }
     }
 
-    // Close the CRC file
+    // Close files
+    close(inputFile);
     close(crcFile);
 }
 
+
+
 int main(int argc, char* argv[]) {
-    // ... (unchanged)
+   // ... (unchanged)
 
-    // New command-line options
-    int generate = 0;
-    int verify = 0;
-    int maxNumErrors = 0;
 
-    for (int i = 2; i < argc; i++) {
-        if (strcmp(argv[i], "-generate") == 0) {
-            generate = 1;
-        } else if (strcmp(argv[i], "-verify") == 0) {
-            verify = 1;
-        } else if (strcmp(argv[i], "-maxNumErrors") == 0) {
-            i += 1;
-            maxNumErrors = atoi(argv[i]);
-        }
-        // ... (Previous options remain unchanged)
-    }
+   // New command-line options
+   int generate = 0;
+   int verify = 0;
+   int maxNumErrors = 0;
 
-    // Call the appropriate function based on command-line arguments
-    if (generate) {
-        generateCRCFile(argv[1]);
-    }
 
-    if (verify) {
-        verifyCRCFile(argv[1], maxNumErrors);
-    }
+   for (int i = 2; i < argc; i++) {
+       if (strcmp(argv[i], "-generate") == 0) {
+           generate = 1;
+       } else if (strcmp(argv[i], "-verify") == 0) {
+           verify = 1;
+       } else if (strcmp(argv[i], "-maxNumErrors") == 0) {
+           i += 1;
+           maxNumErrors = atoi(argv[i]);
+       }
+       // ... (Previous options remain unchanged)
+   }
 
-    // ... (Rest of the code remains unchanged)
 
-    return 0;
+   // Call the appropriate function based on command-line arguments
+   if (generate) {
+       generateCRCFile(argv[1]);
+   }
+
+
+   if (verify) {
+       verifyCRCFile(argv[1], maxNumErrors);
+   }
+
+
+   // ... (Rest of the code remains unchanged)
+
+
+   return 0;
 }
-*/
 
