58ed76cfe565c2702891eab7e55d54fb9a589894
Claudia Hereter <claudia.hereter01@estudiant.upf.edu>
2024-02-26 21:27:14 +0100

    n=4 no va

--- P3/main.c
+++ P3/main.c
@@ -13,11 +13,13 @@ FileManager fm;
 
 void* worker_function(void * arg){
     crc* crc = malloc(sizeof(crc));
+
     while (fm.nFilesRemaining>0){
         unsigned char buff[256];
-        //crc* crc;
         dataEntry  d;
-        getAndReserveFile(&fm, &d); // Reserves a file (it will later be released)
+        d.fddata = -1;
+        d.fdcrc = -1;
+        int res = getAndReserveFile(&fm, &d); // Reserves a file (it will later be released)
 
         if (d.fddata != -1 && d.fdcrc != -1) {  // firstly we should check if file is successfully reserved
             read(d.fdcrc, crc, sizeof(*crc));
@@ -28,20 +30,26 @@ void* worker_function(void * arg){
                 printf("CRC error in file %d\n", d.index);
             }
 
-
             unreserveFile(&fm, &d);     // it is important to unreserve the file and it's done here!
             markFileAsFinished(&fm, &d);
 
             // we have decided to add a sloop before processing the next file
             sleep(1);
         }
+        if(res==1 && fm.nFilesRemaining==0){
+            free(crc);
+            pthread_exit(NULL);
+        }
     }
     free(crc);
-
-    exit(0);
+    return NULL;
 }
 
 int main(int argc, char ** argv) {
+    // start a timer
+    startTimer(0);
+    long timeTotal = 0;
+
     // initialize FileManager
     initialiseFdProvider(&fm, argc, argv);
     // initialize the semaphore
@@ -57,8 +65,11 @@ int main(int argc, char ** argv) {
     for (int i = 0; i < N; ++i) {
         pthread_join(threadID[i], NULL);
     }
+
     // finally destroy the fileManager resources
     destroyFdProvider(&fm);
 
+    timeTotal += endTimer(0);
+    printf("total time taken: %ld \n", timeTotal);
     exit(0);
 }
